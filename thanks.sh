9f1a26387e174c9547c7ccf6fcd85f85cc9c527a
rudrapratap <145328489+rudrapratap9990@users.noreply.github.com>
2023-10-27 17:12:10 +0000

    Create thanks.sh

---
+++ thanks.sh
@@ -0,0 +1,26 @@
+@@ -0,0 +1,25 @@
+cyn='\e[0;36m'
+
+lgrn='\e[1;32m'
+
+r='\e[1;31m'
+thank.sh
+y='\e[1;33m'
+################
+echo ' 
+ 
+                 _|    _|            _|  _|            
+                 _|    _|    _|_|    _|  _|    _|_|    
+                 _|_|_|_|  _|_|_|_|  _|  _|  _|    _|  
+                 _|    _|  _|        _|  _|  _|    _|  
+                 _|    _|    _|_|_|  _|  _|    _|_|' | lolcat
+echo " "
+echo " "
+cat letter.txt
+echo " "
+echo "                      " ✿ MY SELF RUDRAPRATAP ✿ | lolcat
+echo " " 
+echo " "
+sleep 16.0
+clear 
+exit 0 
